4b05607627fec193464ae5e45c0555a1d224eaf3
C1Banks <129791524+C1Banks@users.noreply.github.com>
2023-10-05 10:59:37 -0700

    jest test

--- silksonic-songbook/server/app.test.js
+++ silksonic-songbook/server/app.test.js
@@ -1,3 +1,11 @@
-it('should not fail', () => {
-    expect(true).toBe(false);
+
+//import '@testing-library/jest-dom';
+//import test from 'node:test';
+
+
+//import { render, screen } from '@testing-library/react';
+
+
+test('should not fail', () => {
+    expect(true).toBe(true);
 });
